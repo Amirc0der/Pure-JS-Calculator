4ff7e3f08a2561d43ef3dbac3cbecb4c27fdc15f
Amirreza Heydari <93425589+Amirc0der@users.noreply.github.com>
2021-11-25 17:41:02 +0330

    Update arx_slider.js

--- arx_slider.js
+++ arx_slider.js
@@ -1,4 +1,4 @@
-const timerlength = 10000 // Change it if you want to change the autoslide speed (in miliseconds, 10000 means 10 seconds) 
+const timerlength = 5000 // Change it if you want to change the autoslide speed (in miliseconds, 10000 means 10 seconds) 
 const slider = document.querySelector(".slider");
 const sliderImg = slider.querySelectorAll("img");
 const sliderPreviusButton = slider.querySelector(".left")
@@ -8,7 +8,6 @@ const sliderCount = slider.querySelectorAll("img").length - 1
 
 let currentSlider = 0;
 let dotsnum = sliderCount + 1;
-
 // CreateDots function creates clickable dots on the bottom of the slider based on the number of images
 function CreateDots () {
 	if ( dotsnum != 0 ) {
@@ -22,7 +21,6 @@ function CreateDots () {
 	}
 }
 CreateDots()
-
 const sliderdots =  slider.querySelectorAll(".slider_dots")
 function sliderShow () {
 	sliderImg[currentSlider].style.opacity = "1";
@@ -50,11 +48,11 @@ function sliderJump(number) {
 	sliderHide();
 	currentSlider = number;
 	sliderShow();
+	resetTimer()
 }
 
 sliderPreviusButton.addEventListener("click", slidePrevius);
 sliderNextButton.addEventListener("click", slidNext);
-
 sliderShow()
 
 // Auto Play Codes (autoplay is Optional. you can remove the below code if you do not want Autoplay)
@@ -63,15 +61,10 @@ function slideTimer () {
 		slidNext ()
 		window.setTimeout (slideTimer, timerlength) 
 	} else {
-		slideTimerR()
+		sliderJump(0)
+		window.setTimeout (slideTimer, timerlength) 
 	}
 }
-function slideTimerR (){
-	if (currentSlider > 0) { 
-		slidePrevius ()
-		window.setTimeout (slideTimerR, timerlength) 
-	} else {
-		slideTimer()
-	}
+window.setTimeout (slideTimer, timerlength);
+function resetTimer () {	
 }
-window.setTimeout (slideTimer, timerlength) 
