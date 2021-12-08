64b0030216106a12992379c88513612f27f32493
Amirreza Heydari <93425589+Amirc0der@users.noreply.github.com>
2021-12-08 16:28:47 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -1,19 +1,19 @@
-const input = document.getElementById('monitor');
+const input = document.querySelector('#monitor');
 let fix = 0;
 var output = 0;
 var show = 0;
 var mm = 0;
 
 function monitor_update() {
-	input.innerHTML = show;
+	input.value = show;
 }
 
 function add_to_monitor() {
-	input.innerHTML += show;
+	input.value += show;
 }
 
 function calculate() {
-	fix = input.innerHTML
+	fix = input.value
 	fix = fix.replace("++", "+");
 	fix = fix.replace("--", "+");
 	fix = fix.replace("**", "*");
@@ -21,13 +21,13 @@ function calculate() {
 	fix = fix.replace("..", ".");
 	fix = fix.replace(")(", ")*(");
 	output = eval(fix);
-	input.innerHTML = output;
+	input.value = output;
 	show = 0;
 }
 
 function num(x) {
-	if (input.innerHTML == "0") {
-		input.innerHTML = ""
+	if (input.value == "0") {
+		input.value = ""
 	} 
 	show = x;
 	add_to_monitor();
@@ -58,7 +58,7 @@ function mminus() {
 }
 
 function clean() {
-	input.innerHTML = 0;
+	input.value = 0;
 }
 
 document.addEventListener('keydown', function(event) {
@@ -67,9 +67,9 @@ document.addEventListener('keydown', function(event) {
 		num(mykey);
 
 	} else if (event.keyCode == 8) {
-		nummer = input.innerHTML;
+		nummer = input.value;
 		nummer = nummer.substring(0, nummer.length - 1);
-		input.innerHTML = nummer;
+		input.value = nummer;
 	} else if (event.keyCode == 13) {
 		calculate();
 	}
