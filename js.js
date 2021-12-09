0cc0aa7d6687d79e1c6c6cf97f5422b36dacfd3c
Amirreza Heydari <93425589+Amirc0der@users.noreply.github.com>
2021-12-09 18:40:42 +0330

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
 const input = document.querySelector('#monitor');
 let fix = 0;
-let output = 0;
 let memory = 0;
 input.value = 0;
 
@@ -10,8 +9,8 @@ function calculate() {
 		fix = fix.replace(i + '(', i + '*(')
 	}
 	fix = fix.replace(")(", ")*(");						// fixing multipilications like (4)(2)
-	output = eval(fix);
-	input.value = output;
+	input.value = eval(fix);
+	return eval(fix);
 }
 function num(x) {
 	if (input.value == "0") {
@@ -19,7 +18,7 @@ function num(x) {
 	}
 	input.value += x;
 }
-function operator(y) {										// preventing user from writing two operators in a row
+function operator(y) {									// preventing user from typing two operators in a row
 	let opr = input.value.charAt(input.value.length - 1)
 	if (opr != "-" && opr != "*" && opr != "+" && opr != "/" && opr != ".") {
 		input.value += y;
@@ -32,25 +31,23 @@ function mclean() {
 	memory = 0;
 }
 function mplus() {
-	calculate();
-	memory += output;
+	memory += calculate();
 }
 function memoryinus() {
-	calculate();
-	memory -= output;
+	memory -= calculate();
 }
 function clean() {
 	input.value = 0;
 }
-									// Codes below adresses the key on the keyboard to the above functions
+												// Codes below adresses the key on the keyboard to the above functions
 document.addEventListener('keydown', function(event) {
-	if (event.keyCode > 95 && event.keyCode < 106) {						// numpad keys
+	if (event.keyCode > 95 && event.keyCode < 106) {					// numpad keys
 		let mykey = event.keyCode - 96;
 		num(mykey);
 
-	} else if (event.keyCode == 8) { 								 // backspace
+	} else if (event.keyCode == 8) { 							 // backspace
 		input.value = input.value.substring(0, input.value.length - 1);
-	} else if (event.keyCode == 13) {	 							// enter
+	} else if (event.keyCode == 13) {	 						// enter
 		calculate();
 	}
 	switch (event.keyCode) {
